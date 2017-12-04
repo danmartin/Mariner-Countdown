b5a25ea6f6b6e18353bdefeb1f0261c0b8c2e88d
Dan Martin <dmartin@geiger.com>
2017-12-04 08:33:45 -0800

    Date change for pitchers/catchers

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,12 @@ var app = angular.module("app", []);
 
 app.controller('MainCtrl', ['$scope',  function($scope) {
     var year = 2018,
-        end = new Date('02/15/'+ year.toString() +' 00:01 AM'),
+        end = new Date('02/14/'+ year.toString() +' 00:01 AM'),
         now = new Date(),
         _day = 86400000;
     if((end - now) < 0){
         year++;
-        end = new Date('02/15/'+ year.toString() +' 00:01 AM');
+        end = new Date('02/14/'+ year.toString() +' 00:01 AM');
     }
     console.log(_day);
     $scope.days = Math.floor((end - now) / _day);
